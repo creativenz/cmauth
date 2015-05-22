735ba20b9f851eb162f9e82b64b10f8095ae7374
dollardad <kevin@bestnet.co.nz>
2015-05-22 14:42:39 +1200

    added unit test for sessions

--- Cmauth.php
+++ Cmauth.php
@@ -42,6 +42,7 @@ class Cmauth {
 	 */
 	public function log_user_in($username = null, $password = null)
 	{
+
 		// Unit tests - username and password have been passed to function
 		$this->_CI->unit->run($username, 'is_string', 'Login username exists');
 		$this->_CI->unit->run($password, 'is_string', 'Login Password exists');
@@ -52,14 +53,10 @@ class Cmauth {
 			// Get the user if they exist
 			$query = $this->_CI->db->limit(1)->where('username', $username)->where('active', 1)->get('users');
 
-			// Unit test for query is it should return an object
-			if(empty($query))
-			{
-				$error = $this->_CI->db->error();
-
-			}
-			$notes = isset($error) ? $error['message'] : 'No db errors'  ;
-			$this->_CI->unit->run($query, 'is_object', 'Login db query for username and password', $notes);
+			/**** Unit test for query is it should return an array ****/
+			$error = $this->_CI->db->error();
+			$this->_CI->unit->run($query, 'is_object', 'Login db query for username and password', $error['message']);
+			/**** end unit test ****/
 
 			if($query) {
 
@@ -74,11 +71,30 @@ class Cmauth {
 						$data = array( 
 							'last_logged_in' => date('Y-m-d H:i:s'),
 						);
-						$this->_CI->db->where('id', $user['id'])->update('users', $data);
+						$query = $this->_CI->db->where('id', $user['id'])->update('users', $data);
+						$rows_affected = $this->_CI->db->affected_rows();
+
+						/**** Unit testing ****/ 
+
+						// Did the db query work
+						$error = $this->_CI->db->error();
+						$this->_CI->unit->run($query, 'is_true', 'Valid db query to update last login', $error['message']);
+
+						// Has a row been updated
+						$this->_CI->unit->run($rows_affected, 1, 'Updated db last login', 'User last login ' . $user['username']);
+
+						/**** End UNit Testing ****/
+
+						// Destroy all previous sessions and start from scratch
+						$this->_CI->session->sess_destroy();
 
 						// Set up a generic session for user.  Access control session should be set up
 						// as an application session as it would be different for each web site
 						$this->_CI->session->set_userdata($user);
+
+						/**** Unit Testing - check that the user session has been set ****/
+						$this->_CI->unit->run($this->_CI->session->id, 'is_numeric', 'User session set', 'Has the user id session been set');
+						/**** end unit testing ****/
 
 						return $user;
 					}
